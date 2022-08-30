a756b4ec698d2ce740b0b8fd4c7e797cc6ee0990
jcamilorg <juancamilorg64@gmail.com>
2022-08-30 11:23:48 -0500

    Creacion de como llegar y actualizaciones en directorio corporativo

--- src/components/pages/Nosotros/ComoLlegar.jsx
+++ src/components/pages/Nosotros/ComoLlegar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../layouts/LayoutBannerBreadcrumb";
+import Iframe from "../../utils/Iframe";
 import TitleCar from "../../utils/TitleCar";
 
 export default function ComoLlegar() {
@@ -12,6 +13,31 @@ export default function ComoLlegar() {
       <TitleCar>
         <b>¿Cómo</b> llegar?
       </TitleCar>
+      <div className="px-5 py-5">
+        <div className="px-5 mx-5 pb-5">
+          <Iframe link="https://www.youtube.com/embed/wR4jxPt8krk"></Iframe>
+          <div className="text-main fs-responsive-m py-3">
+            60 años por la vida en el territorio
+          </div>
+        </div>
+
+        <h3 className="fw-bold text-acua fs-responsive-l">¿Cómo llegar?</h3>
+        <div className="fs-responsive-m text-main">
+          Como antecedentes que tuvo en cuenta el legislador: para expedir la
+          Ley 3a. de 1961 se deben resaltar: creación de la CVC y la creación de
+          la Corporación Autónoma Regional de los Valles del Magdalena y el
+          Sinú, la cual en 1968 fue transformada para dar nacimiento al
+          Instituto Nacional de Recursos Naturales Renovables Inderena. <br />
+          <br />
+          El proyecto de ley fue presentado por el representante Enrique Pardo
+          Parra quien planteó la iniciativa ante la Cámara, y como ponente fue
+          designado Aurelio Camacho Rueda, quien amplió las funciones
+          consignadas para la nueva Corporación Autónoma Regional en el proyecto
+          original. En el Senado fue designado como ponente el senador Virgilio
+          Barco Vargas, y luego de su paso por el Congreso, la ley fue
+          sancionada por el presidente Alberto Lleras Camargo.
+        </div>
+      </div>
     </Layout>
   );
 }
--- src/components/pages/Nosotros/DirectorioCorporativo.jsx
+++ src/components/pages/Nosotros/DirectorioCorporativo.jsx
@@ -12,7 +12,7 @@ const TableDirectoriesContainerHeader = styled.div`
 
   .header-title {
     background-color: #017185;
-    font-weight: bold;
+    font-weight: 900;
     color: #fff;
     border-right: solid 2px #fff;
     padding: 0 20px;
@@ -26,6 +26,11 @@ const TableDirectoriesContainerBody = styled.div`
   .header {
     border-left: 3px solid #89d335;
     padding-left: 10px;
+    margin: 8px 10px;
+    font-weight: bold;
+  }
+  .header-number {
+    padding-left: 10px;
     margin: 8px 0;
     font-weight: bold;
     margin: 0 10px;
@@ -33,6 +38,7 @@ const TableDirectoriesContainerBody = styled.div`
   .item {
     padding-left: 15px;
     margin: 0 10px;
+    border-bottom: solid 1px #ccc;
   }
   .box-container {
     //box-shadow: 1px 1px 5px #ccc;
@@ -45,51 +51,82 @@ const TableDirectoriesContainerBody = styled.div`
     grid-template-columns: 66% 33%;
   }
 `;
+const TableItems = styled.div`
+  display: grid;
+  grid-template-columns: 33.3% 16.6% 16.6% 16.6% 17%;
+  padding: 5px 0;
+  border-radius: 10px 10px 0 0;
+  grid-row-gap: 15px;
+
+  .item {
+    border: 1px solid #ccc;
+    margin: 0 10px;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header {
+    border-left: 3px solid #89d335;
+    padding-left: 10px;
+    margin: 8px 10px;
+    font-weight: bold;
+  }
+  .card-info {
+    border: 1px solid #ccc;
+    margin: 0 10px;
+  }
+  .card-city {
+    border-left: 3px solid #89d335;
+    padding-left: 10px;
+    margin: 8px 10px;
+    font-weight: bold;
+  }
+`;
 
 const DireccionAreaInfo = () => (
-  <div className="box-container table-info border">
-    <div className="header text-acua">Sede Central</div>
-    <div className="header text-acua">580 11 11</div>
+  <div className="box-container table-info border fs-responsive-xs">
+    <div className="header text-acua fs-responsive-s">Sede Central</div>
+    <div className="header-number text-main text-center">580 11 11</div>
 
     <div className="item text-main">Dirección General</div>
-    <div className="item text-main">Ext: 1000</div>
+    <div className="item text-main text-center">Ext: 1000</div>
 
     <div className="item text-main">Secretaria General</div>
-    <div className="item text-main">Ext: 1100</div>
+    <div className="item text-main text-center">Ext: 1100</div>
 
     <div className="item text-main">
       Dirección de Control Disciplinario Interno
     </div>
-    <div className="item text-main">Ext: 1200</div>
+    <div className="item text-main text-center">Ext: 1200</div>
 
     <div className="item text-main">Oficina de Control Interno</div>
-    <div className="item text-main">Ext: 1300</div>
+    <div className="item text-main text-center">Ext: 1300</div>
 
     <div className="item text-main">
       Oficina de las Tecnologías de la Información y las Comunicaciones
     </div>
-    <div className="item text-main">Ext: 1400</div>
+    <div className="item text-main text-center">Ext: 1400</div>
 
     <div className="item text-main">Oficina de Talento Humano</div>
-    <div className="item text-main">Ext: 1500</div>
+    <div className="item text-main text-center">Ext: 1500</div>
   </div>
 );
 
-const TelefonosInfo = () => (
-  <div className="box-container border">
-    <div className="header text-acua">580 11 11</div>
-    <div className="item text-main">Ext: 1000</div>
-    <div className="item text-main">Ext: 1100</div>
-    <div className="item text-main">Ext: 1200</div>
-    <div className="item text-main">Ext: 1300</div>
-    <div className="item text-main">Ext: 1400</div>
-    <div className="item text-main">Ext: 1500</div>
+const CardInfo = ({ title, cities }) => (
+  <div className="card-info">
+    <div className="header text-acua fs-responsive-s">{title}</div>
+    {cities ? (
+      <>
+        <div className="card-city">Municipios de su jurisdicción:</div>
+        <div className="card-city fw-normal">{cities}</div>
+      </>
+    ) : null}
   </div>
 );
 
 const TableDirectories = () => (
-  <div className="pt-5 px-4">
-    <TableDirectoriesContainerHeader>
+  <>
+    <TableDirectoriesContainerHeader className="fs-responsive-xs">
       <div className="header-title">Dirección o área</div>
       <div className="header-title text-center">Teléfono</div>
       <div className="header-title text-center">Dirección</div>
@@ -98,9 +135,34 @@ const TableDirectories = () => (
     </TableDirectoriesContainerHeader>
     <TableDirectoriesContainerBody>
       <DireccionAreaInfo />
-      <TelefonosInfo />
+      <div className="text-main box-container border fs-responsive-xs text-center">
+        Av. Esperanza # 60-50
+        <br /> Centro Empresarial Gran Estación - Costado Esfera, pisos 6 y 7
+        Bogotá.
+      </div>
+      <div className="text-main box-container border fs-responsive-xs text-center">
+        sau@car.gov.co
+      </div>
+
+      <div className="text-main box-container border fs-responsive-xs ">
+        Lunes a viernes:
+        <br /> 8:00 a.m. a 5:00 p.m.
+      </div>
     </TableDirectoriesContainerBody>
-  </div>
+  </>
+);
+
+const RowInfo = ({ title, cities, number, ext, location, mail, schedule }) => (
+  <>
+    <CardInfo title={title} cities={cities}></CardInfo>
+    <div className="item">
+      {number ? number : "580 11 11"}
+      <br /> Ext. {ext}
+    </div>
+    <div className="item">{location}</div>
+    <div className="item">{mail}</div>
+    <div className="item">{schedule}</div>
+  </>
 );
 
 export default function DirectorioCorporativo() {
@@ -113,7 +175,49 @@ export default function DirectorioCorporativo() {
       <TitleCar>
         <b>Directorio áreas y Direcciones Regionales CAR</b>
       </TitleCar>
-      <TableDirectories></TableDirectories>
+      <div className="pt-5 px-4">
+        <TableDirectories />
+        <TableItems className="text-main fs-responsive-xs">
+          <RowInfo
+            title="Dirección de Laboratorio e Innovación Ambiental"
+            ext="4300"
+            location="Centro Empresarial Santo Domingo (Avenida Troncal de Occidente
+            #18-76) Manzana C - Bodega 13 Mosquera, Cundinamarca"
+            mail="laboratorio@car.gov.co"
+            schedule={
+              <>
+                Lunes a viernes: <br />
+                8:00 a.m. a 5:00 p.m.
+              </>
+            }
+          />
+          <RowInfo
+            title="Casa CAR - Centro de Documentación Ambiental"
+            ext="4200"
+            location="Carrera 20 #37 - 34 Barrio Teusaquillo"
+            mail="cendoc@car.gov.co"
+            schedule={
+              <>
+                Lunes a viernes: <br />
+                8:00 a.m. a 5:00 p.m.
+              </>
+            }
+          />
+          <RowInfo
+            title="Dirección Regional Bogotá - La Calera"
+            cities="Bogotá y La Calera."
+            ext="4200"
+            location="Carrera 20 #37 - 34 Barrio Teusaquillo"
+            mail="cendoc@car.gov.co"
+            schedule={
+              <>
+                Lunes a viernes: <br />
+                8:00 a.m. a 5:00 p.m.
+              </>
+            }
+          />
+        </TableItems>
+      </div>
     </Layout>
   );
 }
